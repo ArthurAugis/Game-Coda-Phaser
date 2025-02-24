1437eb9ee53a58909abe70799971e2a852807eb5
Arthur Augis <92651106+ArthurAugis@users.noreply.github.com>
2025-02-24 10:10:48 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { MainGameScene as MainGame } from './scenes/MainGameScene.ts';
+import { AUTO, Game, Scale,Types } from 'phaser';
+
+//  Find out more information about the MainGameScene Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        MainGame
+    ]
+};
+
+export default new Game(config);
